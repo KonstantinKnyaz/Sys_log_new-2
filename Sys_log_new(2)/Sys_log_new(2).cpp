eaef72b16880ed69edc0392326dcec84e3c6bacb
Константин Князев <79729833+KonstantinKnyaz@users.noreply.github.com>
2021-03-11 17:39:31 +0300

    Добавьте файлы проекта.

---
+++ Sys_log_new(2)/Sys_log_new(2).cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+#include "files.h"
+
+using namespace std;
+
+class User {
+private:
+	string log{};
+	string pass{};
+public:
+	User(string log, string pass) {
+		this->log = log;
+		this->pass = pass;
+	}
+	void showme() {
+		cout << "Логин: " << log << endl << "Пароль: " << pass << endl;
+	}
+	bool acces(User outputObj) {
+		if (log == outputObj.log && pass == outputObj.pass)
+			return true;
+		else
+			return false;
+	}
+};
+
+User adm("Admin", "Key666");
+//const User adm2("User", "c1PP");
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	string log, pass;
+
+	cout << "Введите логин: ";
+	cin >> log;
+	cout << "Введите пароль: ";
+	cin >> pass;
+
+	User uss(log, pass);
+
+	bool acc = false;
+	acc = adm.acces(uss);
+	//acc = adm2.acces(adm);
+
+	if (acc)
+		cout << "Доступ разрешён" << endl;
+	else cout << "Доступ запрещён!" << endl;
+
+	//adm.showme();
+
+	if (acc)
+		file();
+
+	return 0;
+
+}
+
---
+++ Sys_log_new(2)/files.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <clocale>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int file() {
+
+	ofstream file;
+	string file_name;
+	char text[100];
+	cout << "Введите название файла: ";
+	cin >> file_name;
+
+	file.open(file_name, ofstream::app);
+
+	if (file.is_open()) {
+		cout << "Введите текст: ";
+		cin.get();
+		cin.getline(text, sizeof(text));
+		file << text << "\n";
+	}
+	else
+		cout << "Файл не был открыт!" << endl;
+
+	file.close();
+	return 0;
+}
